d9cd2afdd5f3603bb8bf03c311dc0abb7d77b91a
Saharsh Laud <saharshlaud123@gmail.com>
2024-08-25 16:08:40 +0530

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,59 @@
 #include "ast.h"
 #include <fstream>
 #include <iostream>
+using namespace std;
 
 extern ASTNode* root;
 extern FILE* yyin;
 extern int yylex();
 extern int yyparse();
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        std::cerr << "Format: " << argv[0] << " input.tex output.md" << std::endl;
+/**
+ * @brief This is the entry point of the program.
+ * 
+ * Here we are parsing by reading an input LaTeX file and generating an AST.
+ * The AST is then traversed to produce a corresponding Markdown file.
+ * In case any file opening error or parsing error occurs then it is also handled.
+ */
+
+int main(int argc, char **argv) 
+{
+    if(argc != 3) 
+    {
+        cerr<<"Format: "<<argv[0]<<" input.tex output.md"<<endl;
         return 1;
     }
 
     yyin = fopen(argv[1], "r");
-    if (!yyin) {
-        std::cerr << "Error: Cannot open file " << argv[1] << std::endl;
+    if(!yyin) 
+    {
+        cerr<<"Error: Cannot open file "<<argv[1]<<endl;
         return 1;
     }
 
-    int parseResult = yyparse();
-    if (parseResult == 0 && root != nullptr) {
-        std::cout << "Parsing has been successful. AST structure:" << std::endl;
+    int finalparse = yyparse();
+    if(finalparse == 0 && root != nullptr) 
+    {
+        cout<<"Parsing has been successful. AST structure:"<<endl;
         printAST(root);
 
-        std::ofstream outFile(argv[2]);
-        if (!outFile) {
-            std::cerr << "Error: Cannot open " << argv[2] << " for writing" << std::endl;
+        ofstream outFile(argv[2]);
+        if(!outFile)
+        {
+            cerr<<"Error: Cannot open "<<argv[2]<<" for writing"<<endl;
             return 1;
         }
 
         traverseAST(root, outFile);
-        std::cout << "Markdown output generated successfully in " << argv[2] << std::endl;
-    } else {
-        std::cerr << "Error: Parsing failed or AST root is null." << std::endl;
+        cout<<"Markdown output generated successfully in "<<argv[2]<<endl;
+    } 
+    else 
+    {
+      cerr<<"Error: Either Parsing has failed or root of AST is null."<<endl;
     }
 
-    if (yyin) {
+    if(yyin) 
+    {
         fclose(yyin);
     }
 
